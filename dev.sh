4c955ea3d30472b5887b6a9b9b5989a87d08f482
Tim Perry <pimterry@gmail.com>
2016-12-05 19:08:51 +0100

    Add comments to test script, and dev script for quick test iteration

---
+++ dev.sh
@@ -0,0 +1,2 @@
+# Run this file (with 'entr' installed) to watch files and rerun tests on changes
+ls -d **/* | entr ./test.sh
--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
+# Run this file to run all the tests, once
 ./test/libs/bats/bin/bats test/*.bats
