27c4902805f7f16a9a61661641b8e7271570d381
primis <nick@primis.org>
2016-12-07 11:50:54 -0500

    Added Configuration Test

--- test/helpers.bash
+++ test/helpers.bash
@@ -8,11 +8,14 @@ refute_exists() {
 
 setupNotesEnv() {
   export NOTES_DIRECTORY="$(mktemp -d)"
+  export NOTES_HOME="$(mktemp -d)"
+  export HOME=$NOTES_HOME
 }
 
 teardownNotesEnv() {
   if [ $BATS_TEST_COMPLETED ]; then
     rm -rf $NOTES_DIRECTORY
+    rm -rf $NOTES_HOME
   else
     echo "** Did not delete $NOTES_DIRECTORY, as test failed **"
   fi
