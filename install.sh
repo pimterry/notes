07667ce6d4dead7402b993db2378a220f417ff88
primis <nick@primis.org>
2016-12-21 12:33:41 -0500

    Updated the Readme. Bugfix on install.sh too. s Signed-off-by: primis <nick@primis.org>

--- install.sh
+++ install.sh
@@ -12,12 +12,12 @@ function assertInstalled() {
     done
 }
 
-# Pre-setup check for bash completion directory
-bash_completion_dir=`pkg-config --variable=completionsdir bash-completion` 2>/dev/null
-
 # Yay, Echo self documents! :D
 echo "Checking for root..."
     [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
+# Get user's home directory so we can dum config later
+user_home=$(getent passwd $SUDO_USER | cut -d: -f6)
+
 
 echo "Checking for Dependencies..."
     assertInstalled bash curl tar
@@ -26,16 +26,19 @@ echo "Downloading and Extracting Notes from Repository..."
     extract_dir=$(mktemp -d)
     curl -L https://api.github.com/repos/pimterry/notes/tarball | tar -xzp -C $extract_dir --strip-components=1
 
+# Pre-setup check for bash completion directory
+bash_completion_dir=`pkg-config --variable=completionsdir bash-completion` 2>/dev/null
+
 echo "Moving Files into Place..."
-    mv "$extract_dir"/notes /usr/local/bin/notes
+    mv $extract_dir/notes /usr/local/bin/notes
     # Make it runnable
     chmod a+x /usr/local/bin/notes
     # Do we have bash completion abilities?
-    if [ -d "$bash_completion_dir" ]; then
-        mv "$extract_dir"/notes.bash_completion "$bash_completions_dir"/notes
+    if [ -d $bash_completion_dir ]; then
+        mv $extract_dir/notes.bash_completion $bash_completion_dir/notes
     fi
-    mkdir -p ~/.config/notes/
-    mv "$extract_dir"/config.example ~/.config/notes/config.example
+    mkdir -p $user_home/.config/notes/
+    mv $extract_dir/config.example $user_home/.config/notes/config.example
 
 echo "Cleaning Up..."
     rm -rf $extract_dir
