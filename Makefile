4ec4ebf89fb6798dcb89c4b6e0e37f1de2f0b5d5
primis <nick@primis.org>
2017-01-04 13:32:55 -0500

    Added Makefile to make compatible with bpkg.io

---
+++ Makefile
@@ -0,0 +1,20 @@
+PREFIX ?= /usr/local
+BASH_COMPLETION_DIR := $(shell pkg-config --variable=completionsdir bash-completion) 
+# pkg-config adds a space for some reason, we have to strip it off.
+BASH_COMPLETION_DIR := $(strip BASH_COMPLETION_DIR)
+USERDIR ?= $(HOME)
+
+install: 
+	@if [ -d $(BASH_COMPLETION_DIR) ]; then \
+	 	echo $(BASH_COMPLETION_DIR);\
+		cp notes.bash_completion "$(BASH_COMPLETION_DIR)/notes";\
+	fi # Small test for bash completion support
+	install -m755 -D notes $(PREFIX)/bin/notes
+	install -m777 -D config.example $(USERDIR)/.config/notes/config.example
+	install -D notes.1 $(PREFIX)/share/man/man1/notes.1
+	mandb 1>/dev/null
+unintstall:
+	rm -f $(PREFIX)/bin/notes
+	rm -f $(PREFIX)/share/man/man1/notes.1
+	rm -f $(BASH_COMPLETION_DIR)/notes
+	rm -f $(USERDIR)/.config/notes/config.example
--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ echo "Checking for root..."
 
 # Make sure we have everything actually installed that we need to install this.
 echo "Checking for Dependencies..."
-    assertInstalled bash curl tar mktemp
+    assertInstalled bash curl tar mktemp install make
 
 # Variable Definitions go here. 
 user_home=`eval echo ~$SUDO_USER`
@@ -26,22 +26,8 @@ extract_dir=$(mktemp -d /tmp/notes.XXXXX)
 
 echo "Downloading and Extracting Notes from Repository..."
     curl -L https://api.github.com/repos/pimterry/notes/tarball | tar -xzp -C $extract_dir --strip-components=1
-
-echo "Moving Files into Place..."
-    mv $extract_dir/notes /usr/local/bin/notes
-    # Make it runnable
-    # Do we have bash completion abilities?
-    if [ -d $bash_completion_dir ]; then
-        mv $extract_dir/notes.bash_completion $bash_completion_dir/notes
-    fi
-    mkdir -p $user_home/.config/notes/
-    mv $extract_dir/config.example $user_home/.config/notes/config.example
-    mv $extract_dir/notes.1 /usr/local/share/man/man1/notes.1
-echo "Fixing Permissions..."
-    chown -R $SUDO_USER $user_home/.config/notes 
-    chmod a+x /usr/local/bin/notes
-echo "Installing Man page..."
-    mandb
+echo "Installing notes..."
+    cd $extract_dir && make USER_DIR=$user_home
 echo "Cleaning Up..."
     rm -rf $extract_dir
 echo "All done."
