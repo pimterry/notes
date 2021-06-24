2c80d3a0122b8137e2c848f3d52457647a01d327
Tim Perry <pimterry@gmail.com>
2021-06-24 16:12:14 +0200

    Make test script independent of pwd

--- test.sh
+++ test.sh
@@ -1,2 +1,7 @@
-# Run this file to run all the tests, once
+#!/usr/bin/env bash
+
+# Run this file to run all the tests in test/*.bats
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
 ./test/libs/bats/bin/bats test/*.bats
